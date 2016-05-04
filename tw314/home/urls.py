6542c47845e6c0717872db4365893a5607fc5be4
Mylena Maria Silva dos Santos <myleena.santos@hotmail.com>
2016-05-04 20:10:05 -0300

    Master Page e Inicio da tela de Usuário para Administrador.

--- tw314/home/urls.py
+++ tw314/home/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^admin/cadastrar_usuario', views.cadastro_usuario),
 ]
--- tw314/home/views.py
+++ tw314/home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'home/index.html', {})
 
+
+def cadastro_usuario(request):
+    return render(request, 'home/admin/cadastrar_usuario.html', {})
+
