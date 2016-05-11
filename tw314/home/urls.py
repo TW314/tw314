50d394bc48bb760a0d73fbb73419c856b8e891de
HaluDeCassia <haluanedecassia@gmail.com>
2016-05-11 20:12:23 -0300

    DOING - Principal do suporte (alterando nomes)

--- tw314/home/urls.py
+++ tw314/home/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include, url
 from . import views
 
-
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^admin/cadastrar_usuario', views.cadastro_usuario),
-    url(r'^admin/admin_principal', views.admin_principal)
+    url(r'^admin/admin_principal', views.admin_principal),
+    url(r'^admin/suporte_principal', views.suporte_princpal),
 ]
--- tw314/home/views.py
+++ tw314/home/views.py
@@ -12,3 +12,9 @@ def cadastro_usuario(request):
 def admin_principal(request):
     return render(request, 'home/admin/admin_principal.html', {})
 
+
+def suporte_princpal(request):
+    return render(request, 'home/suporte/suporte_principal.html', {})
+
+
+
