d48a73a3ca59bb0fbe8dc94cf8ae3cf3c3c16876
Pedro <pedromiguelscian@hotmail.com>
2016-05-13 16:47:25 -0300

    DONE: Organização da urls.py

--- tw314/home/urls.py
+++ tw314/home/urls.py
@@ -7,13 +7,14 @@ urlpatterns = [
     #admin
     url(r'^admin/cadastrar_usuario', views.cadastro_usuario),
     url(r'^admin/admin_principal', views.admin_principal),
-    url(r'^admin/suporte_principal', views.suporte_princpal),
+    url(r'^admin/cadastro_servico', views.cadastro_servico),
     url(r'^admin/relatorio', views.relatorio),
     #suporte
     url(r'^suporte/suporte_principal', views.suporte_princpal),
     url(r'^suporte/cadastro_admin', views.cadastro_admin),
+    url(r'^suporte/cadastro_estabelecimento', views.cadastro_estabelecimento),
 
     url(r'^teste', views.demo_chart),
-    url(r'^admin/cadastro_servico', views.cadastro_servico),
-    url(r'^suporte/cadastro_estabelecimento', views.cadastro_estabelecimento),
+
+
 ]
