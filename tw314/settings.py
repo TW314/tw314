c900866673be409e9db1a145bcbed31c50896e12
HaluDeCassia <haluanedecassia@gmail.com>
2016-07-28 23:36:22 -0300

    DONE: Mudando Banco sqlite3 para banco com conexão MySQL

--- tw314/settings.py
+++ tw314/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'tw314.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+} 
+"""
+DATABASES = {
+    'default': {
+        'NAME': 'tw314',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'OPTIONS': {'autocommit': True,},
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
