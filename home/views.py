e15bf7c04c3de8dfe1298533f29adf868292fcd5
HaluDeCassia <haluanedecassia@gmail.com>
2016-08-29 16:43:03 -0300

    Refatorando código - linha de retorno do update

--- home/views.py
+++ home/views.py
@@ -173,15 +173,16 @@ def suporte_listar_status(request):
 def suporte_editar_status(request, pk):
     # Editar
 
+    status = get_object_or_404(Status, pk=pk)
+
     if request.method == "POST":
-        status = get_object_or_404(Status, pk=pk)
         form = StsForm(request.POST, instance=status)
         if form.is_valid():
             status = form
             status.save()
             return redirect('/suporte/status/')
 
-    status = get_object_or_404(Status, pk=pk)
+
 
     return render(request, 'home/suporte/suporte_editar_status.html', {'status': status})
 
