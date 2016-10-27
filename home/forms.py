b4823d4fe6bb6dcb54c887ea87c643760626899a
mylenasantos <myleena.santos@hotmail.com>
2016-10-27 16:04:09 -0200

    DOING - POST CADASTRO DE RAMO -

--- home/forms.py
+++ home/forms.py
@@ -6,29 +6,11 @@ class StsForm(forms.Form):
 
 
 class EmpForm(forms.Form):
-    nome_fantasia = forms.CharField(max_length=80)
-    razao_social = forms.CharField(max_length=80)
-    nr_cnpj = forms.CharField(max_length=14)
-    logradouro = forms.CharField(max_length=255)
-    nr_logradouro = forms.CharField(max_length=255)
-    cidade = forms.CharField(max_length=100)
-    bairro = forms.CharField(max_length=100)
-    uf = forms.CharField(max_length=2)
-    cep = forms.CharField(max_length=8)
-    telefone = forms.CharField(max_length=11)
-    email = forms.EmailField(max_length=100)
-    nome_responsavel = forms.CharField(max_length=80)
-    cargo_responsavel = forms.CharField(max_length=45)
-    cpf_responsavel = forms.CharField(max_length=11)
-    data_abertura = forms.DateField()
-    data_ativacao = forms.DateField()
-    data_inativacao = forms.DateField()
+    pass
 
 
 class RamForm(forms.Form):
-    nome = forms.CharField(max_length=45, required=True, min_length=3)
-    descricao = forms.CharField(required=True, min_length=5)
-    status_ativacao = forms.CharField(required = True)
+    url = forms.URLField()
 
 
 class UsuFormSuporte(forms.Form):
