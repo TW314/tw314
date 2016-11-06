e7fce453fe66492de31b3425edf5751206ae175a
AlanCamargo <alanhcamargo@gmail.com>
2016-11-05 23:48:56 -0200

    Transferido form da home para modulo de form

--- form/UsuarioForm.py
+++ form/UsuarioForm.py
@@ -1,12 +1,14 @@
 from django import forms
 
 class UsuarioFormSuporte(forms.Form):
-    pass
+    nome = forms.CharField(max_length=80, required=True)
+    email = forms.EmailField(max_length=100, required=True)
+    data_inativacao = forms.DateField(required=False)
+    status_ativacao = forms.CharField(required=True)
+    empresa = forms.CharField(required=True)
 
 class UsuarioFormAdmin(forms.Form):
     nome = forms.CharField(max_length=80, required=True)
     email = forms.EmailField(max_length=100, required=True)
-    data_ativacao = forms.DateField()
-    senha = forms.CharField(max_length=45)
-    data_inativacao = forms.DateField()
+    data_inativacao = forms.DateField(required=False)
     status_ativacao = forms.CharField(required=True)
