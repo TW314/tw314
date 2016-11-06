17769f0f594d1e5a6a37fd2c52a9d79f8def7df3
pedromscian <pedromiguelscian@hotmail.com>
2016-11-06 18:49:05 -0200

    Preparacao do metodo POST de Empresa

--- service/EmpresaService.py
+++ service/EmpresaService.py
@@ -1,8 +1,17 @@
 from persistence import EmpresaPersistence
 
 
+def cadastra(empresa):
+    return EmpresaPersistence.cadastra(empresa)
+
+
+def atualiza(empresa, pk):
+    return EmpresaPersistence.atualiza(empresa, pk)
+
+
 def busca_por_cnpj(cnpj):
     return EmpresaPersistence.busca_por_cnpj(cnpj)
+
 
 def lista():
     return EmpresaPersistence.lista()
