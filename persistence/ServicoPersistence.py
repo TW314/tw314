eb06852ed4cad2bd25ed3fdb10508bb6a1735275
AlanCamargo <alanhcamargo@gmail.com>
2016-11-05 22:38:32 -0200

    arrumado atributo de ramo de atividade

--- persistence/ServicoPersistence.py
+++ persistence/ServicoPersistence.py
@@ -12,7 +12,7 @@ def cadastra(servico):
         ramo_atividade = form.cleaned_data['ramo_atividade']
         sigla = form.cleaned_data['sigla']
         status_ativacao = form.cleaned_data['status_ativacao']
-        data = {'nome': nome, 'descricao': descricao, 'ramo_atividade': ramo_atividade, 'sigla': sigla,
+        data = {'nome': nome, 'descricao': descricao, 'ramoAtividadeId': ramo_atividade, 'sigla': sigla,
                 'status_ativacao': status_ativacao}
         form = requests.post('http://localhost:3000/servico/', json=data)
     else:
