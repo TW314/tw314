a4b94d1eafbaf36432855569b56ab044e5b8c54f
AlanCamargo <alanhcamargo@gmail.com>
2016-11-06 17:24:02 -0200

    empresa por cnpj

--- persistence/EmpresaPersistence.py
+++ persistence/EmpresaPersistence.py
@@ -2,6 +2,6 @@ from pip._vendor import requests
 
 from form.EmpresaForm import EmpresaForm
 
-def busca_por_cnpj(empresa):
-    servico = requests.get('http://localhost:3000/servico/' + empresa).json()
-    return servico
+def busca_por_cnpj(cnpj):
+    empresa = requests.get('http://localhost:3000/empresa/' + cnpj).json()
+    return empresa
