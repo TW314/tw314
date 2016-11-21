33fb1e6551e507d2fbe0ae71d9e11fa844ffab31
mylenasantos <myleena.santos@hotmail.com>
2016-11-21 15:04:00 -0200

    Mudei o tipo do campo da senha de Login

--- form/LoginForm.py
+++ form/LoginForm.py
@@ -3,4 +3,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     email = forms.EmailField(required=True)
-    senha = forms.PasswordInput()
+    senha = forms.CharField(min_length=6, required=True)
